1e466ed5d203729378a43e98d4d820f6ef923c0e
kevin <righttrianglesrkewl3@gmail.com>
2021-07-20 10:20:07 -0700

    working pull from googlesheet to postgres "todd style"

--- hello_google.py
+++ hello_google.py
@@ -3,20 +3,27 @@ import gspread
 import pandas as pd
 from gspread_pandas import Spread, Client
 from oauth2client.service_account import ServiceAccountCredentials
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, Text, String, DateTime
+import datetime
 
 """
 # REMINDER: must share email located in key by pressing "share" button in google sheets to share with the credential email or it wont work!
 """
 
 # input
-cred_json = os.environ['json_path']
-
-class GooglePostgres:
-    def __init__(self, key_path):
+#cred_json = os.environ['json_path']
+cred_json = "/home/batman/Desktop/google_postgres/key/master_key.json"
+# TODO: accept arguments to initialize class with kwawgs
+class GoogleSheetHelper:
+    def __init__(self, key_path, spreadsheetName, sheetName):
         self.scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         self.key_path = key_path
+        self.spreadsheetName = spreadsheetName
+        self.sheetName = sheetName
 
-    def googlesheetToPostgres(cred_json, spreadsheetName, sheetName, db_object):
+    def sheetToDataframe(self):
         pass
 
 """ Function for gathering profile information from the Client"""
@@ -39,7 +46,39 @@ print(type(rows)) # list of dictionaries (ideal!)
 dataframe = pd.DataFrame(rows)
 print(dataframe)
 
+host = "localhost"
+port = 5432
+username = "zelda"
+password = "password"
+database = "godo0" 
 
+db_uri = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+engine = create_engine(db_uri, echo=True)
+
+jobs_df = dataframe
+table_name = 'google_sheet_data'
+current_utc = datetime.datetime.utcnow()
+jobs_df.to_sql(
+    table_name,
+    engine,
+    if_exists='replace',
+    index=False,
+    chunksize=500,
+    dtype={
+        "Username": String(500),
+        "Timezone": String(500),
+        "UTC start": DateTime,
+        "UTC end":  DateTime,
+        "Number": String(500),
+    }
+)
+
+table_df = pd.read_sql_table(
+    table_name,
+    con=engine
+)
+
+print(table_df.head())
 
 # open all sheets and loop through printing each one
 # available_sheets = client.openall()
