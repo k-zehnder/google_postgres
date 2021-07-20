2dc310dab2d6d5c38f5484c1dd810f3b8d37221b
kevin <righttrianglesrkewl3@gmail.com>
2021-07-20 10:54:25 -0700

    working class to get data from individual google sheets within worksheet

--- hello_google.py
+++ hello_google.py
@@ -26,6 +26,12 @@ class GoogleSheetHelper:
     def sheetToDataframe(self):
         pass
 
+    def getDataframe(self):
+        spreadsheet = client.open(self.spreadsheetName)
+        sheet = spreadsheet.worksheet(self.sheetName)
+        rows = sheet.get_all_records()
+        return pd.DataFrame(rows)
+
 """ Function for gathering profile information from the Client"""
 # check data in spreadsheet
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -46,39 +52,62 @@ print(type(rows)) # list of dictionaries (ideal!)
 dataframe = pd.DataFrame(rows)
 print(dataframe)
 
-host = "localhost"
-port = 5432
-username = "zelda"
-password = "password"
-database = "godo0" 
+df1 = GoogleSheetHelper(cred_json, "google_postgres", "existing")
+df2 = GoogleSheetHelper(cred_json, "google_postgres", "calls")
+df3 = GoogleSheetHelper(cred_json, "google_postgres", "time")
 
-db_uri = f"postgresql://{username}:{password}@{host}:{port}/{database}"
-engine = create_engine(db_uri, echo=True)
+print(df1.getDataframe().head())
+print("*" * 50)
+print(df2.getDataframe().head())
+print("*" * 50)
+print(df3.getDataframe().head())
 
-jobs_df = dataframe
-table_name = 'google_sheet_data'
-current_utc = datetime.datetime.utcnow()
-jobs_df.to_sql(
-    table_name,
-    engine,
-    if_exists='replace',
-    index=False,
-    chunksize=500,
-    dtype={
-        "Username": String(500),
-        "Timezone": String(500),
-        "UTC start": DateTime,
-        "UTC end":  DateTime,
-        "Number": String(500),
-    }
-)
+###############################################################################
+# Now that we have data from googlesheetsAPI, insert to goanddo PostgresSQL
+###############################################################################
+# host = "localhost"
+# port = 5432
+# username = "zelda"
+# password = "password"
+# database = "godo0" 
 
-table_df = pd.read_sql_table(
-    table_name,
-    con=engine
-)
+# db_uri = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+# engine = create_engine(db_uri, echo=True)
 
-print(table_df.head())
+# jobs_df = dataframe # see above
+# table_name = 'google_sheet_data'
+# current_utc = datetime.datetime.utcnow()
+# jobs_df["CreatedUTC"] = current_utc
+# jobs_df.to_sql(
+#     table_name,
+#     engine,
+#     if_exists='replace',
+#     index=False,
+#     chunksize=500,
+#     dtype={
+#         "Username": String(500),
+#         "Timezone": String(500),
+#         "UTC start": DateTime,
+#         "UTC end":  DateTime,
+#         "Number": String(500),
+#         "CreatedUTC": DateTime
+#     }
+# )
+
+# table_df = pd.read_sql_table(
+#     table_name,
+#     con=engine
+# )
+
+# print(table_df.head())
+
+"""
+Why: there is a limit on the scalability of the architecture in the google spreadsheet. 
+
+It is necessary to exclude all the code for working with Google spreadsheet and replace it with working with a database (PostgreSQL).
+Prepare function Init which will automatically migrate data from the current table structure to a new one do not miss existing data.
+As result, changes should make a backup of the data, create a new database with a new structure, and upload data from the backup.
+"""
 
 # open all sheets and loop through printing each one
 # available_sheets = client.openall()
