b59586f9707896a8ff309e4153d56b4dbe3aa49f
kevin <righttrianglesrkewl3@gmail.com>
2021-07-23 15:24:37 -0700

    update with test

--- init.py
+++ init.py
@@ -29,82 +29,82 @@ print(df3.getDataframe().columns)
 ###############################################################################
 # Now that we have data from googlesheetsAPI, insert to goanddo PostgresSQL
 ###############################################################################
-host = "localhost"
-port = 5432
-username = "zelda"
-password = "password"
-database = "emptydb" 
+# host = "localhost"
+# port = 5432
+# username = "zelda"
+# password = "password"
+# database = "emptydb" 
 
-db_uri = f"postgresql://{username}:{password}@{host}:{port}/{database}"
-engine = create_engine(db_uri, echo=True)
+# db_uri = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+# engine = create_engine(db_uri, echo=True)
 
-# existing sheet inside Users worksheet
-existing_df = df1.getDataframe() #dataframe # see above
-table_name = 'User_existing'
-current_utc = datetime.datetime.utcnow()
-existing_df["CreatedUTC"] = current_utc
-existing_df.to_sql(
-    table_name,
-    engine,
-    if_exists='replace',
-    index=False,
-    chunksize=500,
-)
+# # existing sheet inside Users worksheet
+# existing_df = df1.getDataframe() #dataframe # see above
+# table_name = 'User_existing'
+# current_utc = datetime.datetime.utcnow()
+# existing_df["CreatedUTC"] = current_utc
+# existing_df.to_sql(
+#     table_name,
+#     engine,
+#     if_exists='replace',
+#     index=False,
+#     chunksize=500,
+# )
 
-table_df = pd.read_sql_table(
-    table_name,
-    con=engine
-)
+# table_df = pd.read_sql_table(
+#     table_name,
+#     con=engine
+# )
 
-print(table_df.head())
+# print(table_df.head())
 
-# calls sheet inside Users worksheet
-calls_df = df2.getDataframe() #dataframe # see above
-table_name = 'User_calls'
-current_utc = datetime.datetime.utcnow()
-calls_df["CreatedUTC"] = current_utc
-calls_df.to_sql(
-    table_name,
-    engine,
-    if_exists='replace',
-    index=False,
-    chunksize=500,
-)
+# # calls sheet inside Users worksheet
+# calls_df = df2.getDataframe() #dataframe # see above
+# table_name = 'User_calls'
+# current_utc = datetime.datetime.utcnow()
+# calls_df["CreatedUTC"] = current_utc
+# calls_df.to_sql(
+#     table_name,
+#     engine,
+#     if_exists='replace',
+#     index=False,
+#     chunksize=500,
+# )
 
-table_df = pd.read_sql_table(
-    table_name,
-    con=engine
-)
+# table_df = pd.read_sql_table(
+#     table_name,
+#     con=engine
+# )
 
-print(table_df.head())
+# print(table_df.head())
 
-# time sheet inside Users worksheet
-time_df = df3.getDataframe() #dataframe # see above
-table_name = 'User_time' # + utc for unique backup?
-current_utc = datetime.datetime.utcnow()
-time_df["CreatedUTC"] = current_utc
-time_df.to_sql(
-    table_name,
-    engine,
-    if_exists='replace',
-    index=False,
-    chunksize=500,
-)
+# # time sheet inside Users worksheet
+# time_df = df3.getDataframe() #dataframe # see above
+# table_name = 'User_time' # + utc for unique backup?
+# current_utc = datetime.datetime.utcnow()
+# time_df["CreatedUTC"] = current_utc
+# time_df.to_sql(
+#     table_name,
+#     engine,
+#     if_exists='replace',
+#     index=False,
+#     chunksize=500,
+# )
 
-table_df = pd.read_sql_table(
-    table_name,
-    con=engine
-)
+# table_df = pd.read_sql_table(
+#     table_name,
+#     con=engine
+# )
 
-print(table_df.head())
+# print(table_df.head())
 
-"""
-Why: there is a limit on the scalability of the architecture in the google spreadsheet. 
+# """
+# Why: there is a limit on the scalability of the architecture in the google spreadsheet. 
 
-It is necessary to exclude all the code for working with Google spreadsheet and replace it with working with a database (PostgreSQL).
-Prepare function Init which will automatically migrate data from the current table structure to a new one do not miss existing data.
-As result, changes should make a backup of the data, create a new database with a new structure, and upload data from the backup.
-"""
-"""
-# REMINDER: must share email located in key by pressing "share" button in google sheets to share with the credential email or it wont work!
-"""
+# It is necessary to exclude all the code for working with Google spreadsheet and replace it with working with a database (PostgreSQL).
+# Prepare function Init which will automatically migrate data from the current table structure to a new one do not miss existing data.
+# As result, changes should make a backup of the data, create a new database with a new structure, and upload data from the backup.
+# """
+# """
+# # REMINDER: must share email located in key by pressing "share" button in google sheets to share with the credential email or it wont work!
+# """
--- util.py
+++ util.py
@@ -1,3 +1,13 @@
+# imports from unut
+import os
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, Text, String, DateTime
+import datetime
+
+# util.py imports
 import os
 import gspread
 import pandas as pd
@@ -39,16 +49,92 @@ class GoogleSheetHelper:
         return [sheet.title for sheet in available_sheets]
 
 
-# df1 = GoogleSheetHelper(cred_json, "google_postgres", "existing")
-# df2 = GoogleSheetHelper(cred_json, "google_postgres", "calls")
-# df3 = GoogleSheetHelper(cred_json, "google_postgres", "time")
+df1 = GoogleSheetHelper(cred_json, "google_postgres", "existing")
+df2 = GoogleSheetHelper(cred_json, "google_postgres", "calls")
+df3 = GoogleSheetHelper(cred_json, "google_postgres", "time")
 
 
-# print(df1.getDataframe().head())
-# print("*" * 50)
-# print(df2.getDataframe().head())
-# print("*" * 50)
-# print(df3.getDataframe().head())
+host = "localhost"
+port = 5432
+username = "zelda"
+password = "password"
+database = "test_godo" 
 
-# all_sheets = df1.viewAllClientSheets()
-# print(all_sheets)
+db_uri = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+engine = create_engine(db_uri, echo=True)
+
+
+# get utc now to uniquely identify backups
+now = datetime.datetime.utcnow()
+
+
+# existing sheet inside Users worksheet
+existing_df = df1.getDataframe() #dataframe # see above
+table_name = 'User_existing' + str(now)
+current_utc = datetime.datetime.utcnow()
+existing_df["CreatedUTC"] = current_utc
+existing_df.to_sql(
+    table_name,
+    engine,
+    if_exists='replace',
+    index=False,
+    chunksize=500,
+)
+
+table_df = pd.read_sql_table(
+    table_name,
+    con=engine
+)
+
+print(table_df.head())
+
+# calls sheet inside Users worksheet
+calls_df = df2.getDataframe() #dataframe # see above
+table_name = 'User_calls' + str(now)
+current_utc = datetime.datetime.utcnow()
+calls_df["CreatedUTC"] = current_utc
+calls_df.to_sql(
+    table_name,
+    engine,
+    if_exists='replace',
+    index=False,
+    chunksize=500,
+)
+
+table_df = pd.read_sql_table(
+    table_name,
+    con=engine
+)
+
+print(table_df.head())
+
+# time sheet inside Users worksheet
+time_df = df3.getDataframe() #dataframe # see above
+table_name = 'User_time' + str(now)
+current_utc = datetime.datetime.utcnow()
+time_df["CreatedUTC"] = current_utc
+time_df.to_sql(
+    table_name,
+    engine,
+    if_exists='replace',
+    index=False,
+    chunksize=500,
+)
+
+table_df = pd.read_sql_table(
+    table_name,
+    con=engine
+)
+
+print(table_df.head())
+
+"""
+Why: there is a limit on the scalability of the architecture in the google spreadsheet. 
+
+It is necessary to exclude all the code for working with Google spreadsheet and replace it with working with a database (PostgreSQL).
+Prepare function Init which will automatically migrate data from the current table structure to a new one do not miss existing data.
+As result, changes should make a backup of the data, create a new database with a new structure, and upload data from the backup.
+"""
+"""
+# REMINDER: must share email located in key by pressing "share" button in google sheets to share with the credential email or it wont work!
+"""
